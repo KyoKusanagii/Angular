a35553ae58104c18a97fcf3016dae390dec30740
KyoKusanagii <44694954+KyoKusanagii@users.noreply.github.com>
2019-04-19 14:09:10 +0800

    第一個Angular專案

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MyFirstAngular';
+  months = ['一月','二月','三月','四月','五月','六月','七月'
+    ,'八月','九月','十月','十一月','十二月'];   //月份陣列
+  isavailable = false; //設定變數是否能存取
+
 }
