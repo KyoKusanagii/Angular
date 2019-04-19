cce4a4970767a0ecdf54bc52d28ffe4b30dd3fb6
KyoKusanagii <44694954+KyoKusanagii@users.noreply.github.com>
2019-04-19 17:10:52 +0800

    第一個Angular專案

--- src/app/change-text.directive.ts
+++ src/app/change-text.directive.ts
@@ -1,5 +1,7 @@
 import { Directive ,ElementRef} from '@angular/core';
 
+//屬於屬性類 directive ( Attribute directives )
+//改變元素 ( Element )、元件 ( Component) 或另一個 directive 的外觀或行為
 @Directive({
   selector: '[appChangeText]'
 })
