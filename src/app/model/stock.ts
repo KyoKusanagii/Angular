61be878590a4104f88cf37b61264d28ebd21ec8e
KyoKusanagii <44694954+KyoKusanagii@users.noreply.github.com>
2019-04-25 17:54:22 +0800

    1

--- src/app/model/stock.ts
+++ src/app/model/stock.ts
@@ -1,11 +1,16 @@
+import {Person} from "./person";
+
 export class Stock {
   favorite:boolean = false;
+  notablePeople: Person[];
   constructor(
     public name:string,
     public code:string,
     public price:number,
     public previousPrice:number,
-    public exchange:string){}
+    public exchange:string){
+    this.notablePeople = [];
+  }
 
 
   isPositiveChange():boolean{
